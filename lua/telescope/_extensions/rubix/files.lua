aaa330c902cf94897cbde4b20a056126dde81132
Joshua Rubin <me@jawa.dev>
2024-10-16 00:02:26 +0200

    ignore copilot for root dir
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- lua/telescope/_extensions/rubix/files.lua
+++ lua/telescope/_extensions/rubix/files.lua
@@ -8,11 +8,13 @@ return function(opts)
 	local fname = vim.uri_to_fname(vim.uri_from_bufnr(0))
 	if next(clients) ~= nil then
 		for _, client in ipairs(clients) do
-			if client.root_dir ~= nil then
-				local root_dir = client.root_dir or ""
-				local start = string.find(fname, root_dir, 1, true)
-				if start and start == 1 and string.len(root_dir) > string.len(ret) then
-					ret = root_dir
+			if client.name ~= "copilot" then
+				if client.root_dir ~= nil then
+					local root_dir = client.root_dir or ""
+					local start = string.find(fname, root_dir, 1, true)
+					if start and start == 1 and string.len(root_dir) > string.len(ret) then
+						ret = root_dir
+					end
 				end
 			end
 		end
