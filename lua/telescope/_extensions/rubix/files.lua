dccd50f3149bd84ed87083c38a8695c4c01a1e31
Joshua Rubin <me@jawa.dev>
2024-10-15 11:33:40 +0200

    use lsp to find root dir
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- lua/telescope/_extensions/rubix/files.lua
+++ lua/telescope/_extensions/rubix/files.lua
@@ -1,9 +1,22 @@
 local builtin = require("telescope.builtin")
-local dir = require("rubix/dir")
 
 return function(opts)
 	opts = opts or {}
 	opts.find_command = { "rg", "--files", "--hidden" }
-	opts.cwd = dir.project()
+	local clients = vim.lsp.get_clients({ bufnr = 0 })
+	local ret = ""
+	local fname = vim.uri_to_fname(vim.uri_from_bufnr(0))
+	if next(clients) ~= nil then
+		for _, client in ipairs(clients) do
+			if client.root_dir ~= nil then
+				local root_dir = client.root_dir or ""
+				local start = string.find(fname, root_dir, 1, true)
+				if start and start == 1 and string.len(root_dir) > string.len(ret) then
+					ret = root_dir
+				end
+			end
+		end
+	end
+	opts.cwd = ret
 	builtin.find_files(opts)
 end
